97ff57e4a709e6220dc3485f3579e1859ea893a0
Edilhan <edylhan@gmail.com>
2022-08-09 11:07:33 +0600

    setting

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'rest_framework_simplejwt',
+    'drf_yasg',
+
+    'account',
+    'library',
 ]
 
 MIDDLEWARE = [
